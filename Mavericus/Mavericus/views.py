73f24fc060e6a5f51149c1b16c2ad33776c98186
ravitezu <ravi-teja@live.com>
2014-10-09 00:53:44 +0530

    Added few templates

---
+++ Mavericus/Mavericus/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+def home(request):
+    """ home view which servers the wiki home page """
+    return render(request, 'index.html')
